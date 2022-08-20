e091c8412aa290b538dc6b924838ec9028583ab5
Alexander Spies <alexander.paul.spies@gmail.com>
2022-08-20 13:55:24 +0200

    Use own message type

--- server/src/incoming_message_manager.rs
+++ server/src/incoming_message_manager.rs
@@ -1,6 +1,12 @@
+use futures::StreamExt;
 use redis::{Msg, RedisError};
 use thiserror::Error;
 use warp::Stream;
+
+pub struct ChatMessage {
+    pub channel: String,
+    pub message_text: String,
+}
 
 pub struct IncomingMessageManager {
     redis_url: String,
@@ -11,21 +17,28 @@ impl IncomingMessageManager {
         Ok(IncomingMessageManager { redis_url })
     }
 
-    //TODO: Stream Item should be our own `Msg` type, not `redis::Msg`.
     pub async fn subscribe(
         &mut self,
         channel_name: &str,
-    ) -> IncomingMessageManagerResult<impl Stream<Item = Msg>> {
+    ) -> IncomingMessageManagerResult<impl Stream<Item = IncomingMessageManagerResult<ChatMessage>>>
+    {
         let redis_client = redis::Client::open(self.redis_url.clone())?;
         let connection = redis_client.get_async_connection().await?;
         let mut pubsub = connection.into_pubsub();
 
         pubsub.subscribe(channel_name).await?;
 
-        let stream = pubsub.into_on_message();
+        let stream = pubsub.into_on_message().map(chat_message_from_redis_msg);
 
         Ok(stream)
     }
+}
+
+fn chat_message_from_redis_msg(msg: Msg) -> IncomingMessageManagerResult<ChatMessage> {
+    Ok(ChatMessage {
+        channel: msg.get_channel()?,
+        message_text: msg.get_payload()?,
+    })
 }
 
 #[derive(Error, Debug)]
