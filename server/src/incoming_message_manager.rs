dac816a8d426b008ce2d31be8efb9444da0d87bb
Alexander Spies <alexander.paul.spies@gmail.com>
2022-08-20 13:45:57 +0200

    Make redis url configurable

--- server/src/incoming_message_manager.rs
+++ server/src/incoming_message_manager.rs
@@ -2,13 +2,13 @@ use redis::{Msg, RedisError};
 use thiserror::Error;
 use warp::Stream;
 
-static REDIS_CONNECTION_STRING: &str = "redis://127.0.0.1:6379";
-pub struct IncomingMessageManager {}
+pub struct IncomingMessageManager {
+    redis_url: String,
+}
 
 impl IncomingMessageManager {
-    //TODO: Make redis connection string a parameter here.
-    pub fn new() -> IncomingMessageManagerResult<Self> {
-        Ok(IncomingMessageManager {})
+    pub fn new(redis_url: String) -> IncomingMessageManagerResult<Self> {
+        Ok(IncomingMessageManager { redis_url })
     }
 
     //TODO: Stream Item should be our own `Msg` type, not `redis::Msg`.
@@ -16,7 +16,7 @@ impl IncomingMessageManager {
         &mut self,
         channel_name: &str,
     ) -> IncomingMessageManagerResult<impl Stream<Item = Msg>> {
-        let redis_client = redis::Client::open(REDIS_CONNECTION_STRING)?;
+        let redis_client = redis::Client::open(self.redis_url.clone())?;
         let connection = redis_client.get_async_connection().await?;
         let mut pubsub = connection.into_pubsub();
 
