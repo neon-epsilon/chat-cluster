b24d70703d7a84c910ca65cfa42c615df0939681
Alexander Spies <alexander.paul.spies@gmail.com>
2022-08-28 18:20:29 +0200

    Add commit-SHA to docker image tag

--- k8s/publish_chat_client_image.sh
+++ k8s/publish_chat_client_image.sh
@@ -3,10 +3,12 @@ set -euo pipefail
 IFS=$'\n\t'
 set -x
 
-DOCKER_TAG=chat-client
+SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
+
+CURRENT_COMMIT_SHA=$(cd $SCRIPT_DIR && git rev-parse --short HEAD)
+DOCKER_TAG=chat-client-$CURRENT_COMMIT_SHA
 DOCKER_REPOSITORY=localhost:8050
 
-SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
 CHAT_CLIENT_DIR=$SCRIPT_DIR/../chat-client
 
 docker build -t $DOCKER_REPOSITORY/$DOCKER_TAG -f $CHAT_CLIENT_DIR/Dockerfile $CHAT_CLIENT_DIR
