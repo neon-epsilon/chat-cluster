88b76516f66d31d5b7608b7760b359e0123c00e7
Alexander Spies <alexander.paul.spies@gmail.com>
2023-01-23 22:50:36 +0100

    Implement reqwest-based replication log client

--- rust-workspace/binaries/chat-server/src/replication_log_client.rs
+++ rust-workspace/binaries/chat-server/src/replication_log_client.rs
@@ -7,3 +7,20 @@ use common::ChatMessage;
 pub trait ReplicationLogClient {
     async fn get_messages_for_channel(&self, channel_name: &str) -> Result<Vec<ChatMessage>>;
 }
+
+pub struct ReqwestReplicationLogClient {
+    pub replication_log_url: String,
+}
+
+#[async_trait]
+impl ReplicationLogClient for ReqwestReplicationLogClient {
+    async fn get_messages_for_channel(&self, channel_name: &str) -> Result<Vec<ChatMessage>> {
+        let replication_log_url = &self.replication_log_url;
+        let url = format!("{replication_log_url}/{channel_name}");
+        let body = reqwest::get(url).await?.text().await?;
+
+        let deserialized_messages: Vec<ChatMessage> = serde_json::from_str(&body)?;
+
+        Ok(deserialized_messages)
+    }
+}
